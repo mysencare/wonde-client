abe60703e9f36a6ff7c940e70721df233d0f8625
José Antonio Parra <josparbar@gmail.com>
2024-09-24 17:11:37 +0200

    Add GET endpoint for achievements

--- lib/endpoints/achievements.rb
+++ lib/endpoints/achievements.rb
@@ -1,11 +1,17 @@
 module Wonde
   class Achievements < Endpoints
     @@uri = 'achievements/'
+
     def initialize(token, id=false)
       super(token, id)
       self.uri = @@uri
-      self.uri = id + '/' + @@uri if id
-      self.uri = self.uri.gsub("//", "/").chomp("/")
+      self.uri = "#{id}/#{@@uri}" if id
+      self.uri = uri.gsub('//', '/').chomp('/')
+    end
+
+    def get(id, includes = {}, parameters = {})
+      self.uri = "#{uri}/"
+      super
     end
   end
 end
